23cfd05ba9ff539d53416e13e59a637aa37a7e66
Agustin Sileoni <agustinsileoni98@gmail.com>
2026-01-26 22:10:31 -0300

    Inicializacion del proyecto

---
+++ gestion/src/main/java/com/tugomeria/gestion/GestionGomeriaApplication.java
@@ -0,0 +1,13 @@
+package com.tugomeria.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGomeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionGomeriaApplication.class, args);
+	}
+
+}
---
+++ gestion/src/test/java/com/tugomeria/gestion/GestionGomeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugomeria.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGomeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
